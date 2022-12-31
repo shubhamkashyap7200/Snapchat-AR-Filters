1796f456f568843f11901154808210c1132ddc1a
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2022-12-31 12:21:23 +0530

    checkpoint 1 - Allowing to take screenshot using arview.snapshot

--- AR Funny Face App/ContentView.swift
+++ AR Funny Face App/ContentView.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 import RealityKit
 
+var arView: ARView!
+
 struct ContentView : View {
     // MARK: - Properties
     let sceneCount = 2
-    var arView: ARView!
     
     @State var propId: Int = 0
     
@@ -19,46 +20,66 @@ struct ContentView : View {
         ZStack {
             ARViewController(propId: $propId).edgesIgnoringSafeArea(.all)
             
-            HStack {
-                
-                Spacer()
-
-                // Left Button
-                Button(action: {
-                    self.propId = self.propId <= 0 ? 0 : self.propId - 1
-                    print(propId)
-                }) {
-                    Image(systemName: "arrow.backward.circle.fill")
-                        .foregroundColor(.white)
-                        .font(.system(size: 44.0))
+            Group {
+                HStack {
+                    Spacer()
+                    
+                    // Left Button
+                    Button(action: {
+                        self.propId = self.propId <= 0 ? 0 : self.propId - 1
+                        print(propId)
+                    }) {
+                        Image(systemName: "arrow.backward.circle.fill")
+                            .foregroundColor(.white)
+                            .font(.system(size: 44.0))
+                    }
+                    
+                    Spacer()
+                    
+                    // Screen shot button
+                    Button(action: {
+                        self.takeScreenShot()
+                    }) {
+                        Image(systemName: "circle.fill")
+                            .foregroundColor(.white)
+                            .font(.system(size: 72.0))
+                        //                        .border(.red, width: 2.0)
+                    }
+                    
+                    Spacer()
+                    
+                    // Right Button
+                    Button(action: {
+                        self.propId = self.propId >= sceneCount ? sceneCount : self.propId + 1
+                        print(propId)
+                    }) {
+                        Image(systemName: "arrow.forward.circle.fill")
+                            .foregroundColor(.white)
+                            .font(.system(size: 44.0))
+                    }
+                    
+                    Spacer()
+                    
                 }
-                
-                Spacer()
-                
-                // Screen shot button
-                Button(action: {
-//                    self.takeScreenShot()
-                }) {
-                    Image(systemName: "circle.fill")
-                        .foregroundColor(.white)
-                        .font(.system(size: 72.0))
-//                        .border(.red, width: 2.0)
-                }
-                
-                Spacer()
-                
-                // Right Button
-                Button(action: {
-                    self.propId = self.propId >= sceneCount ? sceneCount : self.propId + 1
-                    print(propId)
-                }) {
-                    Image(systemName: "arrow.forward.circle.fill")
-                        .foregroundColor(.white)
-                        .font(.system(size: 44.0))
-                }
-                
-                Spacer()
-
+            }
+            .padding(.bottom)
+            .frame(maxHeight: .infinity, alignment: .bottom)
+        }
+    }
+    
+    
+    // MARK: - Helper Functions
+    func takeScreenShot() {
+        arView.snapshot(saveToHDR: false) { image in
+            guard let data = image?.pngData() else { return }
+            
+            let compressedImage = UIImage(data: data)
+            
+            if let compressedImage = compressedImage {
+                UIImageWriteToSavedPhotosAlbum(compressedImage, nil, nil, nil)
+            }
+            else {
+                print("DEBUG:: IMAGE NOT SAVED")
             }
         }
     }
@@ -67,7 +88,7 @@ struct ContentView : View {
 struct ARViewController: UIViewRepresentable {
     // MARK: - Properties
     @Binding var propId: Int
-
+    
     func makeUIView(context: Context) -> ARView {
         
         arView = ARView(frame: .zero)
